82316772e79bf1365aecd730e756ed3dfa5f76e4
S Abbott <scott.a.abbott@gmail.com>
2020-06-28 20:50:14 +1200

    added TestCreateAccount funtion to GUI_Tests - test failed

--- BankAccountTest/GUI_Tests.vb
+++ BankAccountTest/GUI_Tests.vb
@@ -1,9 +1,35 @@
 ﻿Imports System.Text
+Imports BankAccounts
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
 <TestClass()> Public Class GUI_Tests
 
-    <TestMethod()> Public Sub TestMethod1()
+    <TestMethod()> Public Sub TestCreateAccount()
+
+        ' Arrange - setup test case
+
+        Dim AccountHolder As String = "Ms I.N.Cognito"
+        Dim AccountNumber As String = "ABCD 890111 11167890"
+        Dim InterestRate As String = "4.3"
+        Dim Balance As String = "10343.82"
+        Dim CountryOfOrigin As String = "Isle Of Man"
+        Dim BF As New BankAccountsForm()
+        BF.SetTextForTesting(AccountHolder, AccountNumber, Balance, InterestRate, CountryOfOrigin)
+
+        ' Act - perform the test
+
+        ' use the information on the Bank Form to create an account
+        BF.CreateAccount()
+        ' get a copy of the arrays of Bank Accounts on the Bank Account form
+        Dim TempAccount() As BankAccount = BF.GetAccounts()
+
+        ' Assert - compare to expected results
+        Assert.AreEqual(AccountHolder, TempAccount(0).GetAccountHolder())
+        Assert.AreEqual(AccountNumber, TempAccount(0).GetAccountNumber())
+        Assert.AreEqual(10343.82, TempAccount(0).GetBalance())
+        Assert.AreEqual(4.3, TempAccount(0).GetInterestRate())
+        Assert.AreEqual(CountryOfOrigin, TempAccount(0).GetCountryOfOrigin())
+
     End Sub
 
 End Class
--- BankAccounts/BankAccountsForm.vb
+++ BankAccounts/BankAccountsForm.vb
@@ -1,4 +1,18 @@
 ﻿Public Class BankAccountsForm
+    ' Class variable declarations go here
+    Private Accounts(4) As BankAccount
+    Private NumAccounts As Integer
+
+    ' Class method definitions go here (Constructors / Getters / Setters)
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Me.NumAccounts = 0
+
+    End Sub
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         Application.Exit()
 
@@ -19,4 +33,9 @@
         SetTextForTesting("One", "Two", "Three", "Four", "Five")
 
     End Sub
+
+    Public Function GetAccounts() As BankAccount()
+        Return Me.Accounts
+    End Function
+
 End Class
